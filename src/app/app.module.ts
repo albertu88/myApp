5568f19b0939fd79ef8c14844d669914b477dfcf
ALBERTO GIMENEZ BEAMONTE <albertu.88@gmail.com>
2023-08-28 20:18:35 +0200

    Revert "add components "navbar "and "sidebar""
    This reverts commit f5e268a4c28cae7f628e8543486bc5a80db0a3ab.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    SidebarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
